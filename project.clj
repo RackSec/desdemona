67df8bb12e5578c762e6e88fbe1d9d1b629ceee2
Laurens Van Houtven <_@lvh.io>
2016-01-28 16:57:15 -0600

    Add cljfmt plugin

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.onyxplatform/onyx "0.8.2"]]
+  :plugins [[lein-cljfmt "0.3.0"]]
   :profiles {:uberjar {:aot [desdemona.launcher.aeron-media-driver
                              desdemona.launcher.launch-prod-peers]}
              :dev {:dependencies [[org.clojure/tools.namespace "0.2.11"]]
