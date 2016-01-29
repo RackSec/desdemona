ec0e9f0db639037dd0d5989b054f2c25563ed046
Laurens Van Houtven <_@lvh.io>
2016-01-28 18:35:27 -0600

    Add description, URL to project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject desdemona "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Data-backed security operations"
+  :url "https://github.com/racksec/desdemona"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
