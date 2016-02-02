7826bfbf629c0815bcb6165f05ae7f24b85dfc68
Laurens Van Houtven <_@lvh.io>
2016-02-02 16:20:14 -0600

    Write the simplest test I can think of

---
+++ test/desdemona/query_test.clj
@@ -0,0 +1,9 @@
+(ns desdemona.query-test
+  (:require
+   [desdemona.query :as q]
+   [clojure.test :refer [deftest is]]))
+
+(deftest query-tests
+  (is (= [{:ip "10.0.0.1"}]
+         (q/query '(== (:ip entry) "10.0.0.1")
+                  [{:ip "10.0.0.1"}]))))
