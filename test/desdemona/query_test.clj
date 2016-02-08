720627d8c692559ceb68bbf362ae2a6c385e2489
Laurens Van Houtven <_@lvh.io>
2016-02-08 11:13:33 -0600

    Refactor logic-query-tests to use `are`

--- test/desdemona/query_test.clj
+++ test/desdemona/query_test.clj
@@ -1,7 +1,7 @@
 (ns desdemona.query-test
   (:require
    [desdemona.query :as q]
-   [clojure.test :refer [deftest is]]))
+   [clojure.test :refer [deftest is are]]))
 
 (def dsl->logic
   @#'desdemona.query/dsl->logic)
@@ -16,9 +16,11 @@
                           [{:ip "10.0.0.1"}]))))
 
 (deftest logic-query-tests
-  (is (= []
-         (q/run-logic-query 'l/fail
-                            [{:ip "10.0.0.1"}])))
-  (is (= [[{:ip "10.0.0.1"}]]
-         (q/run-logic-query '(l/featurec x {:ip "10.0.0.1"})
-                            [{:ip "10.0.0.1"}]))))
+  (are [events query results] (= results (q/run-logic-query query events))
+    [{:ip "10.0.0.1"}]
+    'l/fail
+    []
+
+    [{:ip "10.0.0.1"}]
+    '(l/featurec x {:ip "10.0.0.1"})
+    [[{:ip "10.0.0.1"}]]))
