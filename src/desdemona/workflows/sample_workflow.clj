c54da58d2dc6f561a70509806c2589987f2747a0
Sean Schulte <sirsean@gmail.com>
2016-02-09 12:50:14 -0600

    Don't need to define build-workflow twice.

--- src/desdemona/workflows/sample_workflow.clj
+++ src/desdemona/workflows/sample_workflow.clj
@@ -3,8 +3,6 @@
 ;;; The workflow of an Onyx job describes the graph of all possible
 ;;; tasks that data can flow between.
 
-(defmulti build-workflow :mode)
-
 (defn build-workflow []
   [[:read-lines :extract-line-info]
    [:extract-line-info :prepare-rows]
