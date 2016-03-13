09c5d96179ee10ae17700f99e4abe23655fccac5
Oskar Zabik <oskar.zabik@gmail.com>
2016-03-13 11:41:09 +0100

    adding nrepl middleware (#72)

--- project.clj
+++ project.clj
@@ -26,6 +26,8 @@
                  [hiccup "1.0.5"]
                  [secretary "1.2.3"]
                  [venantius/accountant "0.1.7"]
+                 [com.cemerick/piggieback "0.2.2-SNAPSHOT"]
+                 [org.clojure/tools.nrepl "0.2.12"]
 
                  [org.clojure/core.logic "0.8.10"]
                  [org.clojure/core.match "0.3.0-alpha4"]]
@@ -57,7 +59,12 @@
         :dst "resources/ui/css"}
   :doo {:paths {:karma "node_modules/karma/bin/karma"}}
   :figwheel {:http-server-root "ui"
-             :css-dirs ["resources/ui/css"]}
+             :css-dirs ["resources/ui/css"]
+             :server-port 3449
+             :nrepl-port 7002
+             :nrepl-middleware ["cider.nrepl/cider-middleware"
+                                "refactor-nrepl.middleware/wrap-refactor"
+                                "cemerick.piggieback/wrap-cljs-repl"]}
   :cljfmt {:indents {run [[:inner 0]] ;; core.logic
                      fresh [[:inner 0]]}} ;; core.logic
   :profiles {:uberjar {:aot [desdemona.launcher.aeron-media-driver
