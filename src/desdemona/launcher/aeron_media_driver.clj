3b2c40e9ffd81107fdb5d8bd77a23ccdcc3dfd98
Laurens Van Houtven <_@lvh.io>
2016-03-15 15:39:14 -0500

    Extract run-media-driver!

--- src/desdemona/launcher/aeron_media_driver.clj
+++ src/desdemona/launcher/aeron_media_driver.clj
@@ -12,18 +12,28 @@
    ["-h" "--help"
     "Display a help message"]])
 
+(def ^:private aeron-launch-error-message
+  (str
+   "Error starting media driver. This may be due to a media driver data "
+   "incompatibility between versions. Check that no other media driver "
+   "has been started and then use -d to delete the directory on startup"))
+
+(defn ^:private run-media-driver!
+  [opts]
+  (let [ctx (doto (MediaDriver$Context.)
+              (.dirsDeleteOnStart (-> opts :options :delete-dirs)))]
+    (try (MediaDriver/launch ctx)
+         (catch IllegalStateException ise
+           (throw (Exception. aeron-launch-error-message ise))))))
+
 (defn -main [& args]
   (let [opts (parse-opts args cli-options)
-        {:keys [help delete-dirs]} (:options opts)
-        ctx (cond-> (MediaDriver$Context.)
-              delete-dirs (.dirsDeleteOnStart delete-dirs))
-        media-driver (try (MediaDriver/launch ctx)
-                          (catch IllegalStateException ise
-                            (throw (Exception. "Error starting media driver. This may be due to a media driver data incompatibility between versions. Check that no other media driver has been started and then use -d to delete the directory on startup" ise))))]
+        {:keys [help delete-dirs]} (:options opts)]
     (when help
       (run! (fn [opt]
               (println (clojure.string/join " " (take 3 opt))))
             cli-options)
       (System/exit 0))
+    (run-media-driver!)
     (println "Launched the Media Driver. Blocking forever...")
     (<!! (chan))))
