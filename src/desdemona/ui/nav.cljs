81eec30424714dcd7ba2411d3af09a4c81210158
Oskar Zabik <oskar.zabik@gmail.com>
2016-03-15 16:32:26 +0100

    formatting

--- src/desdemona/ui/nav.cljs
+++ src/desdemona/ui/nav.cljs
@@ -23,13 +23,13 @@
                          :bsStyle "default"
                          :pullRight true
                          :title "last 24h"}
-       [d/menu-item {} "Last 4h"]
-       [d/menu-item {} "Last 8h"]
-       [d/menu-item {} "Last 12h"]
-       [d/menu-item {} "Last week"]
-       [d/menu-item {} "Last month"]
-       [d/menu-item {:divider true}]
-       [d/menu-item {} "Custom"]]
+      [d/menu-item {} "Last 4h"]
+      [d/menu-item {} "Last 8h"]
+      [d/menu-item {} "Last 12h"]
+      [d/menu-item {} "Last week"]
+      [d/menu-item {} "Last month"]
+      [d/menu-item {:divider true}]
+      [d/menu-item {} "Custom"]]
      [:button.btn.btn-primary {:aria-label "Search"}
       [:i.fa.fa-search {:aria-hidden true}]]]]])
 
@@ -46,8 +46,8 @@
     [:nav
      (let [current-page (session/get :current-page)
            active? #(= % current-page)]
-      [:ul.nav.nav-tabs
-       [:li {:class (when (active? dashboard-component) "active")}
-        [:a {:href "/"} "Dashboard"]]
-       [:li {:class (when (active? table-component) "active")}
-        [:a {:href "/table"} "Table"]]])]]])
+       [:ul.nav.nav-tabs
+        [:li {:class (when (active? dashboard-component) "active")}
+         [:a {:href "/"} "Dashboard"]]
+        [:li {:class (when (active? table-component) "active")}
+         [:a {:href "/table"} "Table"]]])]]])
