708de36620466eb5f7d13e5377194540b8f622db
Laurens Van Houtven <_@lvh.io>
2016-03-17 09:44:49 -0500

    Separate declaration from assignment
    export always returns success, so this swallows a potential error from
    getting the hostname.

--- script/run_peers.sh
+++ script/run_peers.sh
@@ -3,5 +3,6 @@ set -o errexit
 set -o nounset
 set -o xtrace
 
-export BIND_ADDR=$(hostname --ip-address)
+export BIND_ADDR
+BIND_ADDR=$(hostname --ip-address)
 exec java -cp /srv/desdemona.jar "desdemona.launcher.launch_prod_peers" $NPEERS
