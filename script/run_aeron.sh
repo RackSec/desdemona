f953ccfcb6680372da1c1df82a710de8793e0c41
Laurens Van Houtven <_@lvh.io>
2016-03-17 16:29:25 -0500

    Delete now-defunct scripts

--- script/run_aeron.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-echo "Setting shared memory for Aeron"
-mount -t tmpfs -o remount,rw,nosuid,nodev,noexec,relatime,size=256M tmpfs /dev/shm
-exec java -cp /srv/desdemona.jar "desdemona.launcher.aeron_media_driver" >>/var/log/aeron.log 2>&1
--- script/run_peers.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -o errexit
-set -o nounset
-set -o xtrace
-
-export BIND_ADDR
-BIND_ADDR=$(hostname --ip-address)
-exec java -cp /srv/desdemona.jar "desdemona.launcher.launch_prod_peers" $N_PEERS
