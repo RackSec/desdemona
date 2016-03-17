5968641a75ba1f47dee9c5aaaa49f0c9edd8ff8d
Laurens Van Houtven <_@lvh.io>
2016-03-17 14:34:49 -0500

    Words in env vars have underscores between them

--- script/run_peers.sh
+++ script/run_peers.sh
@@ -5,4 +5,4 @@ set -o xtrace
 
 export BIND_ADDR
 BIND_ADDR=$(hostname --ip-address)
-exec java -cp /srv/desdemona.jar "desdemona.launcher.launch_prod_peers" $NPEERS
+exec java -cp /srv/desdemona.jar "desdemona.launcher.launch_prod_peers" $N_PEERS
