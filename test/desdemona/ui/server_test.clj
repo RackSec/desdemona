33a6c3db1fa35649a3d66ad10641ffe9007b5b33
Oskar Zabik <oskar.zabik@gmail.com>
2016-03-17 14:10:32 +0100

    adding missing test

--- test/desdemona/ui/server_test.clj
+++ test/desdemona/ui/server_test.clj
@@ -3,6 +3,10 @@
             [desdemona.ui.server :as s]
             [ring.mock.request :refer [request]]))
 
+(deftest page-test
+  (is (= s/page
+         "<!DOCTYPE html>\n<html><html><head><title>Desdemona</title><meta charset=\"utf-8\"><meta content=\"width=device-width, initial-scale=1\" name=\"viewport\"><link href=\"/css/main.css\" rel=\"stylesheet\" type=\"text/css\"></head><body><div id=\"app\"></div><script src=\"/js/main.js\" type=\"text/javascript\"></script></body></html></html>")))
+
 (deftest handler-test
   (let [response (s/handler (request :get "/"))
         second-response (s/handler (request :get "/blah"))]
