b46ddda507f11863241d16b72ae49438c1a126c5
Laurens Van Houtven <_@lvh.io>
2016-03-22 17:23:02 -0500

    Move read-config!, add-shutdown-hook!

--- src/desdemona/launcher/launch_prod_peers.clj
+++ src/desdemona/launcher/launch_prod_peers.clj
@@ -1,8 +1,6 @@
 (ns desdemona.launcher.launch-prod-peers
   (:require [desdemona.launcher.utils :as utils]
-            [aero.core :as aero]
             [taoensso.timbre :as t]
-            [clojure.java.io :as io]
             [onyx.plugin.kafka]
             [onyx.plugin.sql]
             [onyx.plugin.core-async]
@@ -11,14 +9,6 @@
             [desdemona.jobs.sample-submit-job]
             [desdemona.lifecycles.sample-lifecycle])
   (:gen-class))
-
-(defn ^:private read-config!
-  []
-  (aero/read-config (io/resource "config.edn") {:profile :default}))
-
-(defn ^:private add-shutdown-hook!
-  [f]
-  (.addShutdownHook (Runtime/getRuntime) (Thread. f)))
 
 (defn -main [n & args]
   (let [n-peers (Integer/parseInt n)
--- src/desdemona/launcher/utils.clj
+++ src/desdemona/launcher/utils.clj
@@ -1,8 +1,18 @@
 (ns desdemona.launcher.utils
   (:require
-   [clojure.core.async :as a]))
+   [clojure.core.async :as a]
+   [aero.core :as aero]
+   [clojure.java.io :as io]))
 
 (defn block-forever!
   "Blocks forever."
   []
   (a/<!! (a/chan)))
+
+(defn read-config!
+  []
+  (aero/read-config (io/resource "config.edn") {:profile :default}))
+
+(defn add-shutdown-hook!
+  [f]
+  (.addShutdownHook (Runtime/getRuntime) (Thread. f)))
