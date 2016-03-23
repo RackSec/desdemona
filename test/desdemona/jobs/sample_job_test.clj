986cd4f4855809cb5de8c4ac46164be3f7cf0aa5
Laurens Van Houtven <_@lvh.io>
2016-03-23 11:22:13 -0500

    Newlines are your friends

--- test/desdemona/jobs/sample_job_test.clj
+++ test/desdemona/jobs/sample_job_test.clj
@@ -20,12 +20,16 @@
                            [:determine-origin :build-row]
                            [:build-row :prepare-rows]
                            [:prepare-rows :write-lines]]
-        expected-lifecycles [{:lifecycle/task :write-lines :lifecycle/calls :desdemona.lifecycles.logging/log-calls}
-                             {:lifecycle/task :read-lines :lifecycle/calls :desdemona.lifecycles.logging/log-calls}
-                             {:lifecycle/task :write-lines :lifecycle/calls :onyx.plugin.sql/write-rows-calls}
-                             {:lifecycle/task :read-lines :lifecycle/calls :onyx.plugin.kafka/read-messages-calls}]]
     (is (= (map :onyx/name catalog) expected-catalog-names))
     (is (= ((find-task catalog :read-lines) :kafka/topic) "test1"))
+        expected-lifecycles [{:lifecycle/task :write-lines
+                              :lifecycle/calls :desdemona.lifecycles.logging/log-calls}
+                             {:lifecycle/task :read-lines
+                              :lifecycle/calls :desdemona.lifecycles.logging/log-calls}
+                             {:lifecycle/task :write-lines
+                              :lifecycle/calls :onyx.plugin.sql/write-rows-calls}
+                             {:lifecycle/task :read-lines
+                              :lifecycle/calls :onyx.plugin.kafka/read-messages-calls}]]
     (is (thrown? Exception ((find-task catalog :doesnt-exist) :kafka/topic)))
     (is (= ((find-task catalog :write-lines) :sql/table) :logLines))
     (is (= workflow expected-workflow))
