93d3f0427eb1d32395a42e18b5ff7234c43d35e9
Sean Schulte <sirsean@gmail.com>
2016-04-13 14:03:18 -0500

    Kibit

--- src/desdemona/tasks/swift.clj
+++ src/desdemona/tasks/swift.clj
@@ -3,10 +3,9 @@
 (defn add-swift-output
   "Adds a Swift output task to a job"
   [job task opts]
-  (-> job
-      (update :catalog conj (merge {:onyx/name task
+  (update job :catalog conj (merge {:onyx/name task
                                     :onyx/plugin :desdemona.plugins.swift/write-batch
                                     :onyx/type :output
                                     :onyx/medium :swift
                                     :onyx/doc "Write segments to Swift"}
-                                   opts))))
+                                   opts)))
