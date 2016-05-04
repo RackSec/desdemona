b007dcd98bcad74769938e7c3e51b906952c513f
Oskar Zabik <oskar.zabik@gmail.com>
2016-05-04 14:32:09 +0200

    gently wrap table in helpful tags

--- src/desdemona/ui/table.cljs
+++ src/desdemona/ui/table.cljs
@@ -7,4 +7,6 @@
   (let [state-deref @state
         rows (:results state-deref)
         all-keys (-> (mapcat wd/get-all-keys rows) distinct wd/prepare-keys)]
-    (wd/sorted-table all-keys rows session/state)))
+    [:div {:class "container-fluid"}
+     [:div {:class "table-responsive"}
+      [wd/sorted-table all-keys rows session/state]]]))
