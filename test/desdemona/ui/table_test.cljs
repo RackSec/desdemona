07184490727a3bd5401e70d5aabd825a8b959a86
Oskar Zabik <oskar.zabik@gmail.com>
2016-05-04 16:10:25 +0200

    updating tests

--- test/desdemona/ui/table_test.cljs
+++ test/desdemona/ui/table_test.cljs
@@ -1,8 +1,17 @@
 (ns desdemona.ui.table-test
   (:require
    [desdemona.ui.table :as table]
-   [cljs.test :as t :refer-macros [deftest testing is are]]))
+   [cljs.test :as t :refer-macros [deftest testing is are]]
+   [reagent.core :as r]
+   [cljs.core.match :refer-macros [match]]))
 
 (deftest table-component-test
-  (is (= (table/table-component)
-         [:div [:h2 "Table"]])))
+  (let [state (r/atom {:results [{:a 1 :b 2}
+                                 {:a 4 :b 2}]})
+        component (table/table-component state)
+        [table wrapper-class] (match component
+                                [:div {:class "container-fluid"}
+                                 [:div {:class wrapper-class}
+                                  table]]
+                                [table wrapper-class])]
+    (is (= wrapper-class "table-responsive table-sorted"))))
