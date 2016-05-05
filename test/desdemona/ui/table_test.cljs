9cd2a9a4604554a40855c0372dd6e83b6ec4e0c6
Oskar Zabik <oskar.zabik@gmail.com>
2016-05-05 13:24:56 +0200

    better (integration) testing

--- test/desdemona/ui/table_test.cljs
+++ test/desdemona/ui/table_test.cljs
@@ -3,15 +3,41 @@
    [desdemona.ui.table :as table]
    [cljs.test :as t :refer-macros [deftest testing is are]]
    [reagent.core :as r]
+   [reagent.session :as session]
    [cljs.core.match :refer-macros [match]]))
 
+(def isClient (not (nil? (try (.-document js/window)
+                              (catch js/Object e nil)))))
+
+(def rflush r/flush)
+
+(defn add-test-div [name]
+  (let [doc     js/document
+        body    (.-body js/document)
+        div     (.createElement doc "div")]
+    (.appendChild body div)
+    div))
+
+(defn with-mounted-component [comp f]
+  (when isClient
+    (let [div (add-test-div "_testreagent")]
+      (let [comp (r/render-component comp div #(f comp div))]
+        (r/unmount-component-at-node div)
+        (r/flush)
+        (.removeChild (.-body js/document) div)))))
+
 (deftest table-component-test
-  (let [state (r/atom {:results [{:a 1 :b 2}
-                                 {:a 4 :b 2}]})
-        component (table/table-component state)
-        [table wrapper-class] (match component
-                                [:div {:class "container-fluid"}
-                                 [:div {:class wrapper-class}
-                                  table]]
-                                [table wrapper-class])]
-    (is (= wrapper-class "table-responsive table-sorted"))))
+  (let [state {:results [{:a 1 :b 2}
+                         {:a 4 :b 2}]}
+        results-count (count (:results state))
+        component [table/table-component]]
+    (session/swap! merge state)
+    (with-mounted-component component
+     (fn [c div]
+       (let [first-th (.querySelector div "th:first-of-type")
+             first-row-entry (.querySelector div "tr:first-of-type > td")
+             rows-count (.-length (.querySelectorAll div "tbody tr"))]
+         (testing "generated markup"
+           (is (= (.-innerHTML first-th) "A"))
+           (is (= (.-innerHTML first-row-entry) "1"))
+           (is (= rows-count results-count))))))))
